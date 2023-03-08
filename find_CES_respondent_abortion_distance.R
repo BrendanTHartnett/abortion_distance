0abfbdb610bdf05f51a3bdf24764d79e16feec02
Brendan Hartnett <103383061+BrendanTHartnett@users.noreply.github.com>
2023-03-08 13:32:46 -0500

    Update find_CES_respondent_abortion_distance.R

--- find_CES_respondent_abortion_distance.R
+++ find_CES_respondent_abortion_distance.R
@@ -3,25 +3,34 @@ library(dplyr)
 options(digits = 7)
 
 #Read CSV with US zipcodes 
+#found at https://hudgis-hud.opendata.arcgis.com/maps/zip-code-population-weighted-centroids
 dat <- read_csv("ZIP_Code_Population_Weighted_Centroids.csv")
 dat <- subset(dat, dat$USPS_ZIP_PREF_STATE_1221 != "AK" & dat$USPS_ZIP_PREF_STATE_1221 != "GU" & dat$USPS_ZIP_PREF_STATE_1221 != "HI" & dat$USPS_ZIP_PREF_STATE_1221 != "PR" & dat$USPS_ZIP_PREF_STATE_1221 != "VI")
-dat$ZIPS <- as.numeric(dat$STD_ZIP5)
+head(dat)
+dat$ZIPS <- dat$STD_ZIP5
+summary(dat$ZIPS)
 
-#Read CES
-df <- read_csv("CES20_Common_OUTPUT_vv.csv")
-df <- subset(df, !is.na(df$inputzip))
-df$ZIPS <- df$inputzip
-#Remove HI and AK
+#Load 2020 CES
+# Available at https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi%3A10.7910/DVN/E9N6PH
+df <- read_csv("Downloads/CES20_Common_OUTPUT_vv.csv")
+df$ZIPS <- df$lookupzip
+summary(df$ZIPS)
 df <- subset(df, inputstate != 15 & inputstate != 2)
+
+#Remove HI and AK
+#df <- read_csv("abortion_distances")
+#df <- subset(df, inputstate != 15 & inputstate != 2)
 
 #Merge abortion distance zipcodes with CES 2020
 data <- merge(df, dat, by.x = "ZIPS", by.y = "ZIPS")
 
 #Import driving distances
-fdat <- read_csv("ABORTION DATA GIS")
+fdat <- read_csv("ABORTION DISTANCES DATAFRAME")
+head(fdat$LONGITUDE_zip)
 
 #combine distances with CES data
-merged_dat <- merge(data, fdat, by.x = c("LONGITUDE", "LATITUDE"), by.y = c("LONGITUDE_zip", "LATITUDE_zip"))
+merged_df2 <- merge(data, fdat, by.x = c("LONGITUDE", "LATITUDE"), by.y = c("LONGITUDE_zip", "LATITUDE_zip"))
 
 #plot distances of CES respondents to abortion facility
-hist(merged_dat$driving_distance)
+hist(merged_df2$driving_distance)
+table(merged_df2$USPS_ZIP_PREF_STATE_1221)
