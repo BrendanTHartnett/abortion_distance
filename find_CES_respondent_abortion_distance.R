360152db6959e98c889f1312b3badf3e6cc5b79e
Brendan Hartnett <103383061+BrendanTHartnett@users.noreply.github.com>
2023-04-03 10:41:52 -0400

    Update find_CES_respondent_abortion_distance.R

--- find_CES_respondent_abortion_distance.R
+++ find_CES_respondent_abortion_distance.R
@@ -2,10 +2,12 @@ library(readr)
 
 data_clean <- subset(data, !is.na(driving_distance))
 
-
+#Zipcode distances obtained from maps_drive_to_abortion.py
 zip_dat <- read_csv("zipcodes_to_abortion.csv")
-CCES <- read_csv("Downloads/CES22_Common.csv")
+
+#CES Data found at: https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/PR4L8P
+CCES <- read_csv("CES22_Common.csv")
 CCES$numericZIPS <- as.numeric(CCES$lookupzip)
 
-
+#Merge zips_to_abortion.csv with CES22_Common
 merged_data_distances_CCES <- merge(CCES, data_clean, by.x = "numericZIPS", by.y = "ZIPS")
